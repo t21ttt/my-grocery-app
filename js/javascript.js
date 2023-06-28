c0222f57368475172c0e4654495bd610c096a17b
Tefera Molla <tefera2111@gmail.com>
2023-06-27 23:55:37 -0700

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+let searchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+
+}
+let shopingCart=document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+      
+}
